f883c3b9bd5b2b8d3967528cfcf9a21ea0b617c5
JavierGarRod <fgarrod408@g.educaand.es>
2025-12-16 13:14:39 +0100

    Subo js

---
+++ Introduccion_JS/js/myscript.js
@@ -0,0 +1,30 @@
+    //alert("Hello to JS");
+    /*var table="Normal table";
+    let chair="One chair"
+    console.log(table)
+    console.log(chair)*/
+
+    /*Concadenacion de variables
+
+    let testBoolean = false;
+    console.log(testBoolean);
+    let testNumber = 10;
+    console.log(testNumber);
+    let testString ='text';
+    console.log(testString);
+
+    Concadenacion de objetos
+
+    let testBooleanObject=new Boolean(true);
+    console.log(testBooleanObject)
+    let testStringObject=new String("Hola caracola");
+    console.log(testStringObject)*/
+
+
+    let name="Javi";
+    let surname="Garcia";
+    let age=18;
+    let question=`Cuantos años tiene ${name} ${surname}?`;
+    let answer="Él tiene "+age+" años"
+    console.log(question)
+    console.log(answer)
